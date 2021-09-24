6544b4a85f620ab172866950f3f2b13343905a5e
Yuxiang Li <seanliyuxiang@gmail.com>
2021-09-23 22:00:51 -0500

    commit tree 506161652b51550a35c1235b6de8924f5be371bf

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+
+  function onFocusHandler() {
+    console.log('Good!');
+  }
+
+  function onBlurHandler() {
+    console.log('Hey! Eyes on me!');
+  }
+
+  return (
+    <div>
+      <button onFocus={onFocusHandler} onBlur={onBlurHandler}>Eyes on me</button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
